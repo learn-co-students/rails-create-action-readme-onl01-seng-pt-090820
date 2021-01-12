d47cfdba774fa552e68f63a30920397c953f4903
DavidRMorphew <morphewd@umich.edu>
2021-01-11 21:05:04 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,8 +12,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.new(title: params[:title], description: params[:description])
-    post.save
-    redirect_to posts_path
+    @post = Post.new(title: params[:title], description: params[:description])
+    @post.save
+    redirect_to post_path(@post)
   end
 end
