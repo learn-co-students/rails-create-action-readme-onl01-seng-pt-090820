8481d85a63b717b68873c03e9d7dcc9e3ff35c53
Alice Genkin <agenkin1@gmail.com>
2021-01-24 12:50:47 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,5 +11,21 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  # add create method here
+  # before revision
+  # def create
+  #   post = Post.new
+  #   post.title = params[:title]
+  #   post.description = params[:description]
+  #   post.save
+  # end
+  
+  def create
+    @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+    redirect_to post_path(@post)
+  end
+
+ 
 end
